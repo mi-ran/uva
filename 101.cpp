cd8f0e460f61bd609460f1d868de8af3dff5115b
jmr <jmr@synapsoft.co.kr>
2018-07-19 22:52:15 +0900

    main 작업1

--- 101.cpp
+++ 101.cpp
@@ -6,8 +6,22 @@ using namespace std;
 int main() {
 	int size;
 
-	string c1, c2;
+	string str, c1, c2;
 	int n1, n2;
+
+	cin >> size;
+
+
+	while(1) {
+		getline(cin, str);
+		if(str == "quit") {
+			//print
+			return 0;
+		}
+
+		// split string
+		// call move or pile function
+	}
 
 	return 0;
 }
